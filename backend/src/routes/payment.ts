b5d7e0588467874a3339dbf203e58ae81c068681
Alex Tarasov <at146@ya.ru>
2025-11-27 11:08:45 +0300

    feat: вынес createPayment в backend
    Signed-off-by: Alex Tarasov <at146@ya.ru>

--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -1,21 +1,41 @@
-import crypto from "node:crypto";
+import CryptoJS from "crypto-js";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/payment-link", (req, res) => {
-  const { amount, orderId } = req.body;
+router.post("/payment/payment-link", (req, res) => {
+  const { sum, orderId } = req.body || {};
+  if (!sum || !orderId) {
+    return res.status(400).json({ error: "Missing sum or orderId" });
+  }
+  // TODO: проверять на не пустое значение при старте
+  const merchant = process.env.MERCHANT!;
+  const pass1 = process.env.PASS1!;
 
-  const login = process.env.ROBOKASSA_LOGIN!;
-  const pass1 = process.env.ROBOKASSA_PASS1!;
+  const outSum = sum.toFixed(2);
+  const invId = Math.floor(1e9 + Math.random() * 9e9);
+  const desc = "Поездка в зоотакси MyPetDrive";
+  const receiptObj = {
+    sno: "usn_income",
+    items: [
+      {
+        name: desc,
+        quantity: 1,
+        sum: sum,
+        payment_method: "full_payment",
+        payment_object: "service",
+        tax: "none",
+      },
+    ],
+  };
 
-  const signature = crypto
-    .createHash("md5")
-    .update(`${login}:${amount}:${orderId}:${pass1}`)
-    .digest("hex");
-
-  const url = `https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=${login}&OutSum=${amount}&InvId=${orderId}&SignatureValue=${signature}`;
-
+  const receipt = JSON.stringify(receiptObj);
+  const shpParam = `Shp_code=${orderId}`;
+  const signStr = `${merchant}:${outSum}:${invId}:${receipt}:${pass1}:${shpParam}`;
+  const signVal = CryptoJS.MD5(signStr).toString(); // по умолчанию hex
+  // TODO: возможно заменить CryptoJS на crypto
+  // const signVal = crypto.createHash("md5").update(signStr).digest("hex");
+  const url = `https://auth.robokassa.ru/Merchant/Index.aspx?MerchantLogin=${merchant}&OutSum=${outSum}&InvId=${invId}&Description=${encodeURIComponent(desc)}&${shpParam}&Receipt=${encodeURIComponent(receipt)}&SignatureValue=${signVal}`;
   res.json({ url });
 });
 
