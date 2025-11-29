47b42a17c0c4d847da7602cacc524e9644f7276b
Alex Tarasov <at146@ya.ru>
2025-11-29 18:56:09 +0300

    feat: add utilsRouter
    Signed-off-by: Alex Tarasov <at146@ya.ru>

---
+++ backend/src/routes/utils.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+router.get("/utils/health-check", async (_req, res) => {
+  try {
+    return res.json({ status: "ok" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+export default router;
